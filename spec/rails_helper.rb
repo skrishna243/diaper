1522e6e0d08b3b2b8748ba3f6f8a57b3250ee714
Tom Rossi <tom@higherpixels.com>
2019-05-25 09:46:33 -0400

    Allowing the request specs to use the sign_in method.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -102,7 +102,8 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::IntegrationHelpers, type: :system
-
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
