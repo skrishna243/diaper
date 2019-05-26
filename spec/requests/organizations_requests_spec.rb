84dcc7e10761503cdf97569705b11c2e998a27bc
Tom Rossi <tom@higherpixels.com>
2019-05-26 12:55:15 -0400

    Completing the migration of the organizations_controller_spec into a request spec.

--- spec/requests/organizations_requests_spec.rb
+++ spec/requests/organizations_requests_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe "OrganizationRequests", type: :request do
   context "While signed in as a normal user" do
     before do
       sign_in(@user)
-      # get organization_path(default_params)
     end
 
     describe "GET #show" do
@@ -37,7 +36,51 @@ RSpec.describe "OrganizationRequests", type: :request do
         end
       end
     end
-
   end
 
+  context "While signed in as an organization admin" do
+    before do
+      sign_in(@organization_admin)
+    end
+
+    describe "GET #edit" do
+      before { get edit_organization_path(default_params) }
+
+      it "is successful" do
+        expect(response).to have_http_status :ok
+      end
+    end
+
+    describe "PATCH #update" do
+      before do
+        update_params = { organization: { name: "Thunder Pants" } }
+        patch "/#{@organization.id}/manage", params: update_params
+      end
+
+      it "can update name" do
+        expect(response).to redirect_to organization_path(default_params)
+        expect(@organization.name).to eq "Thunder Pants"
+      end
+    end
+
+    context "when attempting to access a different organization" do
+      let(:other_organization_params) do
+        { organization_id: create(:organization).to_param }
+      end
+
+      describe "GET #show" do
+        it "redirects to dashboard" do
+          get organization_path other_organization_params
+          expect(response).to redirect_to dashboard_path(other_organization_params)
+        end
+      end
+
+      describe "GET #edit" do
+        it "redirects to dashboard" do
+          get edit_organization_path other_organization_params
+          expect(response).to redirect_to dashboard_path(other_organization_params)
+        end
+      end
+    end
+  end
 end
