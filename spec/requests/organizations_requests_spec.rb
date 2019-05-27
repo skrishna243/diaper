b45184bdede48363c2d9a2e0054e1fa402cde52b
Tom Rossi <tom@higherpixels.com>
2019-05-27 12:28:54 -0400

    Updating the tests to use a subject rather than a before method

--- spec/requests/organizations_requests_spec.rb
+++ spec/requests/organizations_requests_spec.rb
@@ -9,30 +9,37 @@ RSpec.describe "OrganizationRequests", type: :request do
     end
 
     describe "GET #show" do
-      before { get organization_path(default_params) }
+      subject do
+        get organization_path(default_params)
+        response
+      end
 
       it "is successful" do
-        expect(response).to have_http_status :ok
+        expect(subject).to have_http_status :ok
       end
     end
 
     describe "GET #edit" do
-      before { get edit_organization_path(default_params) }
+      subject do
+        get edit_organization_path(default_params)
+        response
+      end
 
       it "denies access and redirects with an error" do
-        expect(response).to redirect_to dashboard_path(default_params)
+        expect(subject).to redirect_to dashboard_path(default_params)
         follow_redirect!
         assert_select ".alert", /Access Denied/
       end
 
       describe "PATCH #update" do
-        before do
+        subject do
           update_params = { organization: { name: "Thunder Pants" } }
           patch "/#{@organization.id}/manage", params: update_params
+          response
         end
 
         it "denies access" do
-          expect(response).to redirect_to dashboard_path(default_params)
+          expect(subject).to redirect_to dashboard_path(default_params)
           expect(@organization.name).to eq @organization.reload.name
         end
       end
@@ -45,21 +52,25 @@ RSpec.describe "OrganizationRequests", type: :request do
     end
 
     describe "GET #edit" do
-      before { get edit_organization_path(default_params) }
+      subject do
+        get edit_organization_path(default_params)
+        response
+      end
 
       it "is successful" do
-        expect(response).to have_http_status :ok
+        expect(subject).to have_http_status :ok
       end
     end
 
     describe "PATCH #update" do
-      before do
+      subject do
         update_params = { organization: { name: "Thunder Pants" } }
         patch "/#{@organization.id}/manage", params: update_params
+        response
       end
 
       it "can update name" do
-        expect(response).to redirect_to organization_path(default_params)
+        expect(subject).to redirect_to organization_path(default_params)
         expect(@organization.name).to eq "Thunder Pants"
       end
     end
@@ -72,14 +83,14 @@ RSpec.describe "OrganizationRequests", type: :request do
       describe "GET #show" do
         it "redirects to dashboard" do
           get organization_path other_organization_params
-          expect(response).to redirect_to dashboard_path(other_organization_params)
+          expect(subject).to redirect_to dashboard_path(other_organization_params)
         end
       end
 
       describe "GET #edit" do
         it "redirects to dashboard" do
           get edit_organization_path other_organization_params
-          expect(response).to redirect_to dashboard_path(other_organization_params)
+          expect(subject).to redirect_to dashboard_path(other_organization_params)
         end
       end
     end
